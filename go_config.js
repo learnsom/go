1d1723c669b4c59c2ede42d74e0959c35e6ff2c8
learnsom <166076409+learnsom@users.noreply.github.com>
2024-04-05 09:42:42 +1030

    Create go_config.js

---
+++ go_config.js
@@ -0,0 +1,7 @@
+{
+  "name": "Golang learner",
+  "website": "https://twitter.com/Golearner",
+  "description": "Golang learner for backend development",
+  "logo": "https://raw.githubusercontent.com/learnsom/go/master/src/image/testdata/data1.png",
+  "twitter": "https://twitter.com/Golearner"
+}
